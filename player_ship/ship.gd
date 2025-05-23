09aeb781e839f90229bc7b1fde89aadb3e95e5d0
najam awan <najamsk@gmail.com>
2025-05-24 00:27:33 +0300

    ship firing lasers

---
+++ player_ship/ship.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready var left_muzzle: Marker2D = $LeftMuzzle
+@onready var right_muzzle: Marker2D = $RightMuzzle
+@onready var spawner_component: SpawnerComponent = $SpawnerComponent
+@onready var fire_rate_timer: Timer = $FireRateTimer
+@onready var scale_component: ScaleComponent = $ScaleComponent
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	fire_rate_timer.timeout.connect(fire_lasers)
+	pass # Replace with function body.
+
+func fire_lasers() -> void:
+	spawner_component.spawn(left_muzzle.global_position)
+	spawner_component.spawn(right_muzzle.global_position)
+	scale_component.tween_scale()
