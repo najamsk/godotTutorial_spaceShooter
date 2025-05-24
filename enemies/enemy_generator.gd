18bedbb52f5b9f36a7efc18c5bfb38b0822c65da
najam awan <najamsk@gmail.com>
2025-05-24 23:19:58 +0300

    pink enemy

--- enemies/enemy_generator.gd
+++ enemies/enemy_generator.gd
@@ -2,10 +2,13 @@ extends Node2D
 
 @export var GreenEnemyScene: PackedScene
 @export var YellowEnemyScene: PackedScene
+@export var PinkEnemyScene: PackedScene
 
 @onready var spawner_component: SpawnerComponent = $SpawnerComponent
 @onready var green_enemy_spawn_timer: Timer = $GreenEnemySpawnTimer
 @onready var yellow_enemy_spawn_timer: Timer = $YellowEnemySpawnTimer
+@onready var pink_enemy_spawn_timer: Timer = $PinkEnemySpawnTimer
+
 
 var margin = 8
 var screen_width = ProjectSettings.get_setting("display/window/size/viewport_width")
@@ -14,6 +17,7 @@ var screen_width = ProjectSettings.get_setting("display/window/size/viewport_wid
 func _ready() -> void:
 	green_enemy_spawn_timer.timeout.connect(handle_spawn.bind(GreenEnemyScene, green_enemy_spawn_timer))
 	yellow_enemy_spawn_timer.timeout.connect(handle_spawn.bind(YellowEnemyScene, yellow_enemy_spawn_timer))
+	pink_enemy_spawn_timer.timeout.connect(handle_spawn.bind(PinkEnemyScene, pink_enemy_spawn_timer))
 
 
 func handle_spawn(eneme_scene: PackedScene, timer: Timer) -> void:
