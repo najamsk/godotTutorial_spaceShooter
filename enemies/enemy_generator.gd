cc0bfa6e1c2817faf6d941ba7d230a2c042181a5
najam awan <najamsk@gmail.com>
2025-05-24 18:28:11 +0300

    green enemy generator

---
+++ enemies/enemy_generator.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@export var GreenEnemyScene: PackedScene
+
+@onready var spawner_component: SpawnerComponent = $SpawnerComponent
+@onready var green_enemy_spawn_timer: Timer = $GreenEnemySpawnTimer
+
+var margin = 8
+var screen_width = ProjectSettings.get_setting("display/window/size/viewport_width")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	green_enemy_spawn_timer.timeout.connect(handle_spawn.bind(GreenEnemyScene, green_enemy_spawn_timer))
+
+
+func handle_spawn(eneme_scene: PackedScene, timer: Timer) -> void:
+	spawner_component.scene = eneme_scene
+	spawner_component.spawn(Vector2(randf_range(margin, screen_width-margin), -16))
+	timer.start()
+	
