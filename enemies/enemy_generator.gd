c44c435461750450fd51f13e7d7d4d2f3b7acd5c
najam awan <najamsk@gmail.com>
2025-05-25 15:02:29 +0300

    score based difficulty

--- enemies/enemy_generator.gd
+++ enemies/enemy_generator.gd
@@ -3,6 +3,8 @@ extends Node2D
 @export var GreenEnemyScene: PackedScene
 @export var YellowEnemyScene: PackedScene
 @export var PinkEnemyScene: PackedScene
+@export var game_stats: GameStats
+
 
 @onready var spawner_component: SpawnerComponent = $SpawnerComponent
 @onready var green_enemy_spawn_timer: Timer = $GreenEnemySpawnTimer
@@ -15,13 +17,20 @@ var screen_width = ProjectSettings.get_setting("display/window/size/viewport_wid
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	green_enemy_spawn_timer.timeout.connect(handle_spawn.bind(GreenEnemyScene, green_enemy_spawn_timer))
-	yellow_enemy_spawn_timer.timeout.connect(handle_spawn.bind(YellowEnemyScene, yellow_enemy_spawn_timer))
-	pink_enemy_spawn_timer.timeout.connect(handle_spawn.bind(PinkEnemyScene, pink_enemy_spawn_timer))
+	green_enemy_spawn_timer.timeout.connect(handle_spawn.bind(GreenEnemyScene, green_enemy_spawn_timer, 3.0))
+	yellow_enemy_spawn_timer.timeout.connect(handle_spawn.bind(YellowEnemyScene, yellow_enemy_spawn_timer, 5.0))
+	pink_enemy_spawn_timer.timeout.connect(handle_spawn.bind(PinkEnemyScene, pink_enemy_spawn_timer, 10.0))
+	game_stats.score_changed.connect(func(new_score: int):
+		if new_score > 10:
+			yellow_enemy_spawn_timer.process_mode = Node.ProcessMode.PROCESS_MODE_INHERIT
+		if new_score > 50:
+			pink_enemy_spawn_timer.process_mode = Node.ProcessMode.PROCESS_MODE_INHERIT
+	)
 
 
-func handle_spawn(eneme_scene: PackedScene, timer: Timer) -> void:
+func handle_spawn(eneme_scene: PackedScene, timer: Timer, time_offset: float = 1.0) -> void:
 	spawner_component.scene = eneme_scene
 	spawner_component.spawn(Vector2(randf_range(margin, screen_width-margin), -16))
-	timer.start()
+	var spawn_rate = time_offset / (0.5 + (game_stats.score * 0.01))
+	timer.start(spawn_rate * randf_range(0.25, 0.5))
 	
--- enemies/pink_enemy.gd
+++ enemies/pink_enemy.gd
@@ -7,6 +7,9 @@ extends Enemy
 @onready var move_side_state: TimedStateComponent = %MoveSideState
 @onready var pause_state: TimedStateComponent = %PauseState
 @onready var move_side_component: MoveComponent = %MoveSideComponent
+@onready var fire_state: StateComponent = %FireState
+@onready var projectile_spawner_component: SpawnerComponent = %ProjectileSpawnerComponent
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -20,7 +23,15 @@ func _ready() -> void:
 	move_side_component.velocity.x = [-20, 20].pick_random()
 	
 	move_down_state.state_finished.connect(move_side_state.enable)
-	move_side_state.state_finished.connect(pause_state.enable)
+	#move_side_state.state_finished.connect(pause_state.enable)
+	move_side_state.state_finished.connect(func():
+		fire_state.enable()
+		scale_component.tween_scale()
+		projectile_spawner_component.spawn(global_position)
+		fire_state.disable()
+		fire_state.state_finished.emit()
+	)
+	fire_state.state_finished.connect(pause_state.enable)
 	pause_state.state_finished.connect(move_down_state.enable)
 	
 	move_down_state.enable()
